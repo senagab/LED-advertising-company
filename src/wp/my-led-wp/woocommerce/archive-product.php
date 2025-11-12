281acdf3ebbb19f325a9f9c909a3e7e0580837b1
senagab <senagab@gmail.com>
2025-11-12 19:55:32 -0400

    feat: shop being rendered

---
+++ src/wp/my-led-wp/woocommerce/archive-product.php
@@ -0,0 +1,38 @@
+<?php
+// Garante que a query seja de produtos, não da página "Loja"
+if ( ! is_post_type_archive('product') ) {
+    wp_reset_query();
+    query_posts( array(
+        'post_type' => 'product',
+        'posts_per_page' => 12,
+        'post_status' => 'publish'
+    ) );
+}
+?>
+
+
+<main class="loja-container">
+    <h1 class="page-title"><?php woocommerce_page_title(); ?></h1>
+
+    <?php do_action('woocommerce_before_main_content'); ?>
+
+    <?php if ( woocommerce_product_loop() ) : ?>
+
+        <?php woocommerce_product_loop_start(); ?>
+
+            <?php while ( have_posts() ) : the_post(); ?>
+                <?php wc_get_template_part( 'content', 'product' ); ?>
+            <?php endwhile; ?>
+
+        <?php woocommerce_product_loop_end(); ?>
+
+        <?php do_action( 'woocommerce_after_shop_loop' ); ?>
+
+    <?php else : ?>
+
+        <?php do_action( 'woocommerce_no_products_found' ); ?>
+
+    <?php endif; ?>
+
+    <?php do_action('woocommerce_after_main_content'); ?>
+</main>
